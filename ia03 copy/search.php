a4b3569b4b732a6d5c388f89761ff2ac68554284
rllhenson <rllhenson@gmail.com>
2014-10-21 14:00:58 -0400

    practice search

--- ia03 copy/search.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$firstname=$_POST['searchinput'];	
-	print "The dude's first name is $searchinput. <br />";	
-	$button=$_POST['searchbutton'];	
-	print "The button clicked= '$searchbutton'.";
-
-
-?>
