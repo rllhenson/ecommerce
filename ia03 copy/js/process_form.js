3fe4bc8d2a9f087d36453424d2916ade4ffffcf1
Anna Kimball <kimball.anna@knights.ucf.edu>
2014-10-27 23:15:36 -0400

    Updated CSS + JS
    Created JS for the review system. Few tweaks still need to be made, but
    for the most part it's pretty much completed.

---
+++ ia03 copy/js/process_form.js
@@ -0,0 +1,9 @@
+// Submit Form
+// ---------------------------------------------------------------------
+function submitForm(submitreview){
+
+		console.log("Hide form, display div");
+		document.getElementById('review').style.display="none";
+		document.getElementById('reviewsent').style.display="block";		
+
+}
