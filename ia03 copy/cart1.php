3d2cbdfb06e712830a6db090b9b81d6770545a5c
Andy Duong <andydng@hotmail.com>
2014-11-05 21:26:16 -0500

    Fixed change quantity

--- ia03 copy/cart1.php
+++ ia03 copy/cart1.php
@@ -19,6 +19,29 @@ if (isset($_POST['index_to_remove']) && $_POST['index_to_remove'] != "") {
 }
 ?>
 <?php 
+//Change items from cart
+if (isset($_POST['item_to_adjust']) && $_POST['item_to_adjust'] != "") {
+    // execute some code
+	$item_to_adjust = $_POST['item_to_adjust'];
+	$quantity = $_POST['quantity'];
+	$quantity = preg_replace('#[^0-9]#i', '', $quantity); // filter everything but numbers
+	if ($quantity >= 100) { $quantity = 99; }
+	if ($quantity < 1) { $quantity = 1; }
+	if ($quantity == "") { $quantity = 1; }
+  //if quantity > than amount in mysql, print error
+	$i = 0;
+	foreach ($_SESSION["cart_array"] as $each_item) { 
+		      $i++;
+		      while (list($key, $value) = each($each_item)) {
+				  if ($key == "item_id" && $value == $item_to_adjust) {
+					  // That item is in cart already so let's adjust its quantity using array_splice()
+					  array_splice($_SESSION["cart_array"], $i-1, 1, array(array("item_id" => $item_to_adjust, "quantity" => $quantity)));
+				  } // close if condition
+		      } // close while loop
+	} // close foreach loop
+}
+?>
+<?php 
 if (isset($_POST['pid'])) {
     $pid = $_POST['pid'];
 	$wasFound = false;
