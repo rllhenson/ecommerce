5f6cd326413ba4d2ca6595414e75cee99ee212d7
rllhenson <rllhenson@gmail.com>
2014-11-07 15:51:42 -0500

    displayed db for admin page

--- ia03 copy/admin.php
+++ ia03 copy/admin.php
@@ -1,55 +1,88 @@
 <?php
-
-		$pageTitle='FloorFive Admin';
-		include 'includes/header.php';
-		include 'includes/connect_to_mysql.php';
-		session_start(); 
-		error_reporting(E_ALL);
+	$pageTitle='FloorFive Admin';
+	include 'includes/header.php';
+	include 'includes/connect_to_mysql.php';
+	session_start(); 
+	error_reporting(E_ALL);
 
 ?>
 			<div class="adminimage">
 				<div id="admincontent">
-					<h2>Recent Orders</h2>
-					<hr>
-					<div id="orders">
-						<table><tr>
-						<th>order number</th><th>name</th><th>quantity</th><th>price</th><th>order date</th></tr>
-						
-						<tr><td><hr></td><td><hr></td><td><hr></td><td><hr></td><td><hr></td></tr>
-						
-						<tr><td>123456</td><td>Blue Flower Rug</td><td>1</td><td>sale</td><td>10/02/2014</td></tr>
-						<tr><td>123457</td><td>Red Ombre Rug</td><td>2</td><td>full</td><td>10/02/2014</td></tr>
-						<tr><td>123458</td><td>Purple Dyed Rug</td><td>1</td><td>full</td><td>10/06/2014</td></tr>
-						</table>
-						<button type="button">Review All</button>
-					</div>
-					<h2>Out of Stock</h2>
+					<h2>Stock</h2>
 					<hr>
 					<div id="stockstuff">
-						<p>Orange Persian Rug</p><p>RUG-OR-5-5-SQ</p><button type="button">Order More</button>
-						<p>Red Shag Rug</p><p>RUG-RD-4-4-CRL</p><button type="button">Order More</button>
-						<p>Yellow Foral Rug</p><p>RUG-YL-6-4-RCT</p><button type="button">Order More</button>
+					<?php
+					function  showlist($mysqli){
+						$myquery="SELECT * FROM products ORDER BY productid";//added order by productid
+					
+					    $result=$mysqli->query($myquery)
+							or die ($mysqli->error); 
+					
+						$count=$result->num_rows;
+					
+						if($count > 0)
+						{	$output="<table><tr>
+							<th><input type='submit' name='action' value='ID'></th>
+							<th><input type='submit' name='action' value='Name'></th>
+							<th><input type='submit' name='action' value='Description'></th>
+							<th><input type='submit' name='action' value='Category'></th>
+							<th><input type='submit' name='action' value='SKU'></th>
+							<th><input type='submit' name='action' value='Stock'></th>
+							<th><input type='submit' name='action' value='Cost'></th>
+							<th><input type='submit' name='action' value='Price'></th>
+							<th><input type='submit' name='action' value='Image'></th>
+							<th><input type='submit' name='action' value='Weight'></th>
+							<th><input type='submit' name='action' value='Size'></th></tr>";
+							$rownumber=0;
+							$num=0;
+							//$numfind[$idnum] = array(); 
+							while ($row=$result->fetch_assoc())
+							{	
+								$idnum=$row['productid'];
+								$prodname=$row['name'];
+								$proddesc=$row['description'];
+								$cat=$row['category'];
+								$sku=$row['sku'];
+								$stock=$row['stock'];
+								$cost=$row['cost'];
+								$price=$row['price'];
+								$img=$row['image'];
+								$weight=$row['weight'];
+								$size=$row['size'];
+
+								$num++;
+								$output.= "<tr>
+								<td>$idnum</td>
+								<td>$prodname</td>
+								<td>$proddesc</td>
+								<td>$cat</td>
+								<td>$sku</td>
+								<td>$stock</td>
+								<td>$cost</td>
+								<td>$price</td>
+								<td>$img</td>
+								<td>$weight</td>
+								<td>$size</td>
+								</tr>";
+								//array_push($numfind[$idnum],$num);
+							}
+							$output.= "</table> <br />";
+							print $output;
+						} #count
+						else print "There is no rug info in the database.<br />";
+					} #showlist
+					
+					
+
+					showlist($mysqli);
+					?>
+
 					</div>
-					<h2>Current Sales</h2>
-					<hr>
-					<div id="salestuff">
-						<table><tr>
-						<th>name</th><th>seller</th><th>stock</th><th>price</th></tr>
-						<tr><td><hr></td><td><hr></td><td><hr></td><td><hr></td></tr>
-						
-						<tr><td>Blue Flower Rug</td><td>seller</td><td>2</td><td>50%</td></tr>
-						<tr><td>Blue Mum Rug</td><td>seller</td><td>3</td><td>50%</td></tr>
-						<tr><td>Blue Peony Rug</td><td>seller</td><td>1</td><td>50%</td></tr>
-						</table>
-						<button type="button">Change Sales</button>
-					</div>
-					<button type="button">View Accounts</button>
-					<button type="button">View Inventory</button>
-					<button type="button">View Sellers</button>
+					
 				</div>
 			</div>
 
-		</div>;
+		</div>
 		
 
 
--- ia03 copy/includes/search.php
+++ ia03 copy/includes/search.php
@@ -59,7 +59,20 @@
 
 									$num++;
 									
-									
+									// <form target="paypal" action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+									// <!-- Identify your business so that you can collect the payments. -->
+									// <input type="hidden" name="business" value="floor.five@gmail.com">
+									// <!-- Specify a PayPal Shopping Cart Add to Cart button. -->
+									// <input type="hidden" name="cmd" value="_cart">
+									// <input type="hidden" name="add" value="1">
+									// <!-- Specify details about the item that buyers will purchase. -->
+									// <input type="hidden" name="item_name" value="Birthday - Cake and Candle">
+									// <input type="hidden" name="amount" value="3.95">
+									// <input type="hidden" name="currency_code" value="USD">
+									// <!-- Display the payment button. -->
+									// <input type="image" name="submit" border="0" src="https://www.paypal.com/en_US/i/btn/btn_cart_LG.gif" alt="PayPal - The safer, easier way to pay online">
+									// <img alt="" border="0" width="1" height="1" src="https://www.paypal.com/en_US/i/scr/pixel.gif" >
+									// </form> 
 									print "
                     <div class='catalog_item'>
                       <div class='catalog_img'>
