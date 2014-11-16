c6f4b3dd56aadea7f0db6de31f8c2d1af62a8566
rllhenson <rllhenson@gmail.com>
2014-11-15 19:10:54 -0500

    I made page title actually do something

--- ia03 copy/includes/header.php
+++ ia03 copy/includes/header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
-	<title><?php $pageTitle ?></title>
+	<title><?php print $pageTitle; ?></title>
 
 	<link rel="stylesheet" href="css/reset.css"/>
 	<link rel="stylesheet" href="css/edit.css"/>
