138b1f7a0700c1c4b9b6e3603f5408537a1d2a4b
Keegan Sanford <Keegan@deletes-MacBook-Pro.local>
2014-11-17 22:57:46 -0500

    Merge branch 'master'
    Conflicts:
    ia03 copy/includes/connect_to_mysql.php

--- ia03 copy/includes/connect_to_mysql.php
+++ ia03 copy/includes/connect_to_mysql.php
@@ -1,6 +1,22 @@
 <?php  
 $mysqli = new mysqli("sulley.cah.ucf.edu", 'ra072140', 'amilu91', 'ra072140');
+<<<<<<< Updated upstream
 // $mysqli = new mysqli("localhost", 'root', 'chocolateguy1', 'floorfive');
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+//$mysqli = new mysqli("localhost", 'root', 'chocolateguy1', 'floorfive');
+=======
+// $mysqli = new mysqli("localhost", 'root', 'chocolateguy1', 'floorfive');
+>>>>>>> FETCH_HEAD
+=======
+// $mysqli = new mysqli("localhost", 'root', 'chocolateguy1', 'floorfive');
+>>>>>>> FETCH_HEAD
+=======
+// $mysqli = new mysqli("localhost", 'root', 'chocolateguy1', 'floorfive');
+>>>>>>> FETCH_HEAD
+>>>>>>> Stashed changes
 //$mysqli = new mysqli("localhost", 'an787958', '4E685B1987FA464388562A50A41C12DF', 'an787958');
 
 $errnum=mysqli_connect_errno();
--- ia03 copy/includes/header.php
+++ ia03 copy/includes/header.php
@@ -9,6 +9,7 @@
 	<link rel="stylesheet" href="css/reset.css"/>
 	<link rel="stylesheet" href="css/styles.css"/>
 	<script type="text/javascript" src="js/process_form.js"></script>
+    <script type="text/javascript" src="js/google_analytics_tracking.js"></script>
 </head>
 
 <body>
